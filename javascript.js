f0129af07d0c9399dea14ae9df3c1006105c8b7d
helan <helanmah@gmail.com>
2022-07-16 13:08:56 -0700

    added toggle function, fixed deleted button

--- javascript.js
+++ javascript.js
@@ -8,7 +8,7 @@ function Book(title, author, pages, read, position) {
   this.position = position
 }
 
-book_display = document.getElementById("books")
+book_display = document.querySelector("#books")
 
 function bookDisplay() {
 
@@ -18,13 +18,58 @@ function addBook() {
    document.getElementById("author").value, document.getElementById("pages").value,
    document.getElementById("read").checked, myLibrary.length);
   myLibrary.push(newBook);
-  book_display.innerHTML += "<ul>" + "<h2>" + "Title: " + newBook.title + "</h2>"
-  + "<li>" + "Author: " + newBook.author + "</li>" + "<li>" + "Pages: " +
-  newBook.pages + "</li>" + "<li>" + "Read: " + newBook.read + "</li>" +
-  "<button id='exit' onclick=removeBook()>" + 'x' + "</button>" + "</ul>"
-  console.log(newBook.read)
+  if (newBook.read == true) {
+    book_display.innerHTML += "<ul>" + "<h2>" + "Title: " + newBook.title + "</h2>"
+    + "<li>" + "Author: " + newBook.author + "</li>" + "<li>" + "Pages: " +
+    newBook.pages + "</li>" + "<li>" + "Read: " + newBook.read + "</li>" +
+    "<button id='exit'>" + 'x' + "</button>" + "Read: " +
+    "<input type='checkbox' value='yes' id='toggle' checked>" + "</ul>"
+  }
+  else {
+      book_display.innerHTML += "<ul>" + "<h2>" + "Title: " + newBook.title + "</h2>"
+      + "<li>" + "Author: " + newBook.author + "</li>" + "<li>" + "Pages: " +
+      newBook.pages + "</li>" + "<li>" + "Read: " + newBook.read + "</li>" +
+      "<button id='exit'>" + 'x' + "</button>" + "Read: " +
+      "<input type='checkbox' value='yes' id='toggle'>" + "</ul>"
+  }
+  removeBook()
+  toggleRead()
   }
 
+function toggleRead() {
+  var toggle = document.querySelectorAll('#toggle')
+  toggle.forEach((button, i) => {
+    button.addEventListener("click", () => {
+      if (myLibrary[i].read == button.checked) {
+      myLibrary[i].read = !button.checked
+    }
+      else if (myLibrary[i].read != button.checked) {
+      myLibrary[i].read = button.checked
+    }
+      book_display.innerHTML = ""
+      for (i in myLibrary) {
+        if (myLibrary[i].read == true) {
+          book_display.innerHTML += "<ul>" + "<h2>" + "Title: " + myLibrary[i].title + "</h2>"
+          + "<li>" + "Author: " + myLibrary[i].author + "</li>" + "<li>" + "Pages: " +
+          myLibrary[i].pages + "</li>" + "<li>" + "Read: " + myLibrary[i].read + "</li>" +
+          "<button id='exit'>" + 'x' + "</button>" + "Read: " +
+          "<input type='checkbox' value='yes' id='toggle' checked>" + "</ul>"
+          toggleRead()
+          removeBook()
+        }
+        else {
+          book_display.innerHTML += "<ul>" + "<h2>" + "Title: " + myLibrary[i].title + "</h2>"
+          + "<li>" + "Author: " + myLibrary[i].author + "</li>" + "<li>" + "Pages: " +
+          myLibrary[i].pages + "</li>" + "<li>" + "Read: " + myLibrary[i].read + "</li>" +
+          "<button id='exit'>" + 'x' + "</button>" + "Read: " +
+          "<input type='checkbox' value='yes' id='toggle'>" + "</ul>"
+          toggleRead()
+          removeBook()
+        }
+      }
+    })
+  })
+}
 
 function removeBook() {
   var exit = document.querySelectorAll("#exit")
@@ -33,17 +78,30 @@ function removeBook() {
       myLibrary.splice(i, 1)
       book_display.innerHTML = ""
       for (i in myLibrary) {
-        book_display.innerHTML += "<ul>" + "<h2>" + "Title: " + myLibrary[i].title + "</h2>"
-        + "<li>" + "Author: " + myLibrary[i].author + "</li>" + "<li>" + "Pages: " +
-        myLibrary[i].pages + "</li>" + "<li>" + "Read: " + myLibrary[i].read + "</li>" +
-        "<button id='exit' onclick=removeBook()>" + 'x' + "</button>" + "</ul>"
-        console.log(myLibrary)
+        if (myLibrary[i].read == true) {
+          book_display.innerHTML += "<ul>" + "<h2>" + "Title: " + myLibrary[i].title + "</h2>"
+          + "<li>" + "Author: " + myLibrary[i].author + "</li>" + "<li>" + "Pages: " +
+          myLibrary[i].pages + "</li>" + "<li>" + "Read: " + myLibrary[i].read + "</li>" +
+          "<button id='exit'>" + 'x' + "</button>" + "Read: " +
+          "<input type='checkbox' value='yes' id='toggle' checked>" + "</ul>"
+          toggleRead()
+          removeBook()
+        }
+        else {
+          book_display.innerHTML += "<ul>" + "<h2>" + "Title: " + myLibrary[i].title + "</h2>"
+          + "<li>" + "Author: " + myLibrary[i].author + "</li>" + "<li>" + "Pages: " +
+          myLibrary[i].pages + "</li>" + "<li>" + "Read: " + myLibrary[i].read + "</li>" +
+          "<button id='exit'>" + 'x' + "</button>" + "Read: " +
+          "<input type='checkbox' value='yes' id='toggle'>" + "</ul>"
+          toggleRead()
+          removeBook()
+        }
       }
     })
   })
 }
 
 //To Solve:
-//make book cards the same size, make read checkbox start at left edge,
-//make read toggle switch, make delete work on single click,
-//make log in button align properly
+//make book cards the same size (change from list items to divs?), make read checkbox start at left edge,
+//make read toggle switch
+//make log in button align properly, make first letters auto caps
