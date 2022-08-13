29e954b2977e3b54b9aadef5d164ef0c685e1cde
helan <helanmah@gmail.com>
2022-08-13 13:25:33 -0700

    cleaned up code, was able to reduce it by nearly half

--- javascript.js
+++ javascript.js
@@ -1,100 +1,63 @@
 let myLibrary = [];
+let bookInfo = [];
+book_display = document.querySelector("#books");
 
-function Book(title, author, pages, read, position) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
-  this.position = position
-}
-
-book_display = document.querySelector("#books")
-
-function bookDisplay() {
-
-}
-function addBook() {
-  const newBook = new Book(document.getElementById("title").value,
-   document.getElementById("author").value, document.getElementById("pages").value,
-   document.getElementById("read").checked, myLibrary.length);
-  myLibrary.push(newBook);
-  if (newBook.read == true) {
-    book_display.innerHTML += "<ul>" + "<h2>" + "Title: " + newBook.title + "</h2>"
-    + "<li>" + "Author: " + newBook.author + "</li>" + "<li>" + "Pages: " +
-    newBook.pages + "</li>" + "<li>" + "Read: " + "<input type='checkbox' value='yes' id='toggle' checked>"
-    + "</li>" + "<button id='exit'>" + 'x' + "</button>" + "</ul>"
-  }
-  else {
-    book_display.innerHTML += "<ul>" + "<h2>" + "Title: " + newBook.title + "</h2>"
-    + "<li>" + "Author: " + newBook.author + "</li>" + "<li>" + "Pages: " +
-    newBook.pages + "</li>" + "<li>" + "Read: " + "<input type='checkbox' value='yes' id='toggle'>"
-    + "</li>" + "<button id='exit'>" + 'x' + "</button>" + "</ul>"
-  }
-  removeBook()
-  toggleRead()
+const library = () => {
+  const book = (title, author, pages, read, position, checked, display) => {
+    return {title,author,pages,read,position,checked, display}
   }
 
-function toggleRead() {
-  var toggle = document.querySelectorAll('#toggle')
-  toggle.forEach((button, i) => {
-    button.addEventListener("click", () => {
-      if (myLibrary[i].read == button.checked) {
-      myLibrary[i].read = !button.checked
+  const addBook = () => {
+    const newBook = book(document.getElementById("title").value,
+    document.getElementById("author").value, document.getElementById("pages").value,
+    document.getElementById("read").checked, myLibrary.length);
+
+    if (newBook.read == true) {
+      newBook.checked = "checked"
     }
-      else if (myLibrary[i].read != button.checked) {
-      myLibrary[i].read = button.checked
+
+    newBook.display = "<div class='book-container'>" + "<h2>" + "Title: " + newBook.title + "</h2>"
+    + "<p>" + "Author: " + newBook.author + "</p>" + "<p>" + "Pages: " +
+    newBook.pages + "</p>" + "<p>" + "Read: " + "<input type='checkbox' value='yes' id='toggle'" + newBook.checked + ">"
+    + "</p>" + "<button id='exit'>" + 'x' + "</button>" + "</div>"
+
+    myLibrary.push(newBook);
+    bookInfo.push(newBook.display);
+
+    bookDisplay();
+    removeBook();
+    toggleRead();
     }
-      book_display.innerHTML = ""
-      for (i in myLibrary) {
-        if (myLibrary[i].read == true) {
-          book_display.innerHTML += "<ul>" + "<h2>" + "Title: " + myLibrary[i].title + "</h2>"
-          + "<li>" + "Author: " + myLibrary[i].author + "</li>" + "<li>" + "Pages: " +
-          myLibrary[i].pages + "</li>" + "<li>" + "Read: " + "<input type='checkbox' value='yes' id='toggle' checked>"
-          + "</li>" + "<button id='exit'>" + 'x' + "</button>" + "</ul>"
-          toggleRead()
-          removeBook()
-        }
-        else {
-          book_display.innerHTML += "<ul>" + "<h2>" + "Title: " + myLibrary[i].title + "</h2>"
-          + "<li>" + "Author: " + myLibrary[i].author + "</li>" + "<li>" + "Pages: " +
-          myLibrary[i].pages + "</li>" + "<li>" + "Read: " + "<input type='checkbox' value='yes' id='toggle'>"
-          + "</li>" + "<button id='exit'>" + 'x' + "</button>" + "</ul>"
-          toggleRead()
-          removeBook()
-        }
-      }
-    })
-  })
+
+  const bookDisplay = () => {
+    book_display.innerHTML = "";
+    for (i in bookInfo) {
+      book_display.innerHTML += bookInfo[i]
+    }
+    removeBook()
+  }
+
+  const toggleRead = () => {
+    var toggle = document.querySelectorAll('#toggle')
+    toggle.forEach((button, i) => {
+      button.addEventListener("click", () => {
+        if (myLibrary[i].read != button.checked) {
+          myLibrary[i].read = button.checked
+      }})
+      })}
+
+  const removeBook = () => {
+    var exit = document.querySelectorAll("#exit")
+    exit.forEach((x, i) => {
+      x.addEventListener("click", () => {
+        bookInfo.splice(i, 1)
+        bookDisplay()
+      })
+    })}
+return {library, addBook, book, removeBook, toggleRead, bookDisplay}
 }
 
-function removeBook() {
-  var exit = document.querySelectorAll("#exit")
-  exit.forEach((button, i) => {
-    button.addEventListener("click", () => {
-      myLibrary.splice(i, 1)
-      book_display.innerHTML = ""
-      for (i in myLibrary) {
-        if (myLibrary[i].read == true) {
-          book_display.innerHTML += "<ul>" + "<h2>" + "Title: " + myLibrary[i].title + "</h2>"
-          + "<li>" + "Author: " + myLibrary[i].author + "</li>" + "<li>" + "Pages: " +
-          myLibrary[i].pages + "</li>" + "<li>" + "Read: " + "<input type='checkbox' value='yes' id='toggle' checked>"
-          + "</li>" + "<button id='exit'>" + 'x' + "</button>" + "</ul>"
-          toggleRead()
-          removeBook()
-        }
-        else {
-          book_display.innerHTML += "<ul>" + "<h2>" + "Title: " + myLibrary[i].title + "</h2>"
-          + "<li>" + "Author: " + myLibrary[i].author + "</li>" + "<li>" + "Pages: " +
-          myLibrary[i].pages + "</li>" + "<li>" + "Read: " + "<input type='checkbox' value='yes' id='toggle'>" 
-          + "</li>" + "<button id='exit'>" + 'x' + "</button>" + "</ul>"
-          toggleRead()
-          removeBook()
-        }
-      }
-    })
-  })
-}
-
-//To Solve:
-//make book cards the same size (change from list items to divs?), make read checkbox start at left edge,
-//make first letters auto caps
+/*To Solve:
+make book cards the same size, make read checkbox start at left edge,
+make first letters auto caps, form character limit, require forms to be filled
+*/
