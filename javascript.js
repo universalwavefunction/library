6cca3e083ef0f2df062b7f6efb63230d5bbdb5c7
helan <helanmah@gmail.com>
2022-08-27 20:01:21 -0700

    made book cards same size

--- javascript.js
+++ javascript.js
@@ -16,7 +16,7 @@ const library = () => {
       newBook.checked = "checked"
     }
 
-    newBook.display = "<div class='book-container'>" + "<h2>" + "Title: " + newBook.title + "</h2>"
+    newBook.display = "<div class='book-container'>" + "<h2>" + newBook.title + "</h2>"
     + "<p>" + "Author: " + newBook.author + "</p>" + "<p>" + "Pages: " +
     newBook.pages + "</p>" + "<p>" + "Read: " + "<input type='checkbox' value='yes' id='toggle'" + newBook.checked + ">"
     + "</p>" + "<button id='exit'>" + 'x' + "</button>" + "</div>"
@@ -57,6 +57,6 @@ return {library, addBook, book, removeBook, toggleRead, bookDisplay}
 }
 
 /*To Solve:
-make book cards the same size, make read checkbox start at left edge,
+make read checkbox start at left edge,
 make first letters auto caps, form character limit, require forms to be filled
 */
